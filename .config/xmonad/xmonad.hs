7aea60f24598b55f0a319efcbfba53d4c0315f9e
Eli Céspedes <felipe.cespedes@protonmail.com>
2022-06-07 01:05:51 -0400

    catppuccinify

--- .config/xmonad/xmonad.hs
+++ .config/xmonad/xmonad.hs
@@ -28,10 +28,10 @@ import XMonad.Actions.UpdatePointer
 -- import XMonad.Actions.Workscreen
 
 
-myLayout = lessBorders Never $ onWorkspace "<fn=1>\xf7b3</fn>" simpleFloat $  tiled ||| Mirror tiled ||| Full ||| threeCol
+myLayout = lessBorders Never $ onWorkspace "<fn=1>\xf7b3</fn>" simpleFloat $ gaps [(U,5), (R,5), (L,5), (D,5)] $ spacing 5 $  tiled ||| Mirror tiled ||| Full ||| threeCol
   where
     threeCol = magnifiercz' 1.3 $ ThreeColMid nmaster delta ratio
-    tiled    = gaps [(U,5), (R,5), (L,5), (D,5)] $ spacing 5 $ Tall nmaster delta ratio
+    tiled    =  Tall nmaster delta ratio
     nmaster  = 1      -- Default number of windows in the master pane
     ratio    = 1/2    -- Default proportion of screen occupied by master pane
     delta    = 3/100  -- Percent of screen to increment by when resizing panes
@@ -78,17 +78,22 @@ myKeys =
 -- Interact with xmobar
 myPP = def { ppCurrent = xmobarColor "black" "white" }
 mySB = statusBarProp "xmobar" (clickablePP myXmobarPP)
-myWorkspaces = ["<fn=1>\xe795</fn>", "<fn=1>\xfa9e</fn>", "<fn=1>\xf713</fn>", "<fn=1>\xf126</fn>", "<fn=1>\xe5ff</fn>", "<fn=1>\xf001</fn>", "<fn=1>\xf086</fn>", "<fn=1>\xf7b3</fn>", "<fn=1>\xf1f8</fn>"]
+myWorkspaces = ["1 <fn=1>\xe795</fn> ", "2 <fn=1>\xfa9e</fn> ", "3 <fn=1>\xf713</fn> ", "4 <fn=1>\xf126</fn> ", "5 <fn=1>\xe5ff</fn> ", "6 <fn=1>\xf001</fn> ", "7 <fn=1>\xf086</fn> ", "8 <fn=1>\xf7b3</fn> ", "9 <fn=1>\xf1f8</fn> "]
 
 myXmobarPP :: PP
 myXmobarPP = def
     { ppSep             = magenta " • "
     , ppTitleSanitize   = xmobarStrip
-    , ppCurrent         = wrap " " "" . xmobarBorder "Top" "#8be9fd" 2
+    , ppCurrent         = wrap " " "" . xmobarBorder "Top" "#a6e3a1" 6
     , ppHidden          = white . wrap " " ""
     , ppHiddenNoWindows = lowWhite . wrap " " ""
-    , ppUrgent          = red . wrap (yellow "!") (yellow "!")
+    , ppUrgent          = red . wrap (red "!") (red "!")
     , ppOrder           = \[ws, l, _, wins] -> [ws, l, wins]
+    , ppLayout          = (\layout -> case layout of
+                            "Spacing Tall" -> ""
+                            "Spacing Mirror Tall" -> ""
+                            "ThreeCol"    -> ""
+                            "Tabbed"      -> "")
     , ppExtras          = [logTitles formatFocused formatUnfocused]
     }
   where
@@ -98,15 +103,15 @@ myXmobarPP = def
     -- | Windows should have *some* title, which should not not exceed a
     -- sane length.
     ppWindow :: String -> String
-    ppWindow = xmobarRaw . (\w -> if null w then "untitled" else w) . shorten 30
+    ppWindow = xmobarRaw . (\w -> if null w then "untitled" else w) . shorten 20
 
     blue, lowWhite, magenta, red, white, yellow :: String -> String
-    magenta  = xmobarColor "#ff79c6" ""
-    blue     = xmobarColor "#bd93f9" ""
+    magenta  = xmobarColor "#cba6f7" ""
+    blue     = xmobarColor "#89b4fa" ""
     white    = xmobarColor "#f8f8f2" ""
-    yellow   = xmobarColor "#f1fa8c" ""
-    red      = xmobarColor "#ff5555" ""
-    lowWhite = xmobarColor "#bbbbbb" ""
+    yellow   = xmobarColor "#f9e2af" ""
+    red      = xmobarColor "#f38ba8" ""
+    lowWhite = xmobarColor "#b4befe" ""
 
 main :: IO ()
 main = xmonad $ ewmhFullscreen $ ewmh $ withEasySB mySB defToggleStrutsKey $ myConfig
--- .doom.d/config.el
+++ .doom.d/config.el
@@ -6,7 +6,7 @@
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets.
-(setq user-full-name "Felipe Cespedes"
+(setq user-full-name "Eli Cespedes"
       user-mail-address "felipe.cespedes@protonmail.com")
 
 ;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
@@ -25,7 +25,8 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-gruvbox)
+;;(setq doom-theme 'catppucin-theme)
+(setq doom-theme 'catppuccin)
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
