8ad05267fe0c9d678447b8c96e0ecba9bca664ad
Eli Céspedes <felipe.cespedes@protonmail.com>
2022-06-24 19:49:17 -0400

    modified install script

--- .local/sillyscripts/install doom.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-git clone https://github.com/hlissner/doom-emacs ~/.config/emacs
-
-~/.config/emacs/bin/doom install
-
---
+++ .local/sillyscripts/install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#Package list
+
+paru --needed --ask 4 -Sy - < pkglist.txt
+
+systemctl --user enable pipewire.service
+systemctl --user enable syncthing.service
+systemctl enable auto-cpufreq --now
+
+stow 
+#Not on arch
+#doom emacs
+#initialize xmonad config
+# git clone xmonad, xmonad-contrib 
+## stack init stack install
+
+stack init & 
+#Doom
+git clone https://github.com/hlissner/doom-emacs ~/.config/emacs
+
+~/.config/emacs/bin/doom install
+
+#Options
+gsettings set org.gnome.desktop.interface color-scheme prefer-dark
+
